2c95e8f589ff22648a98c846606b193c9e1ae2a0
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2025-06-05 20:00:12 +0530

    Update redis configuiration for cloud instance

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,6 +4,8 @@ const redis = new Redis({
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: process.env.REDIS_PORT || 6379,
   },
+  username: "default",
+  password: process.env.REDIS_PASSWORD, 
 });
 
 redis.on("error", (err) => console.error("Redis client error", err));
